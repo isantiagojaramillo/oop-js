93f0c77e9fdd083674e4cce5ed4479458e453bae
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-15 09:30:51 -0500

    intro

--- intro.js
+++ intro.js
@@ -17,3 +17,9 @@ console.log("");
 
 console.log(typeof new Date());
 console.log(typeof new RegExp());
+
+console.log("");
+
+console.log( new Number(100));
+console.log( new String("Hello World 2"));
+console.log( new Boolean(true));
