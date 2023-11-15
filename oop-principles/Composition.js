e211853a596371fd86198a3b7c4e9fe9d8de7f97
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-15 13:11:33 -0500

    composition

---
+++ oop-principles/Composition.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Lex',
+    lastName: 'Luthor',
+    address: {
+        street: "123 bakery street",
+        city: "London",
+        country: "UK"
+    }
+}
+
+// A COMPONENT DOES NOT HAVE INDEPENDANT LIFE WHEN IT BELONGS
+// TO ANOTHER ONE.
