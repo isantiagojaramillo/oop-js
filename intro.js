a0109d522b6ac80d7f5acf7216a464a55becb64e
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-15 09:23:17 -0500

    First commit

--- intro.js
+++ intro.js
@@ -1,3 +1,19 @@
 console.log("Hello World");
 console.log(100);
 console.log(false);
+
+console.log("");
+
+console.log(typeof "Hello World");
+console.log(typeof 100);
+console.log(typeof false);
+
+console.log("");
+
+console.log(typeof {});
+console.log(typeof []);
+
+console.log("");
+
+console.log(typeof new Date());
+console.log(typeof new RegExp());
