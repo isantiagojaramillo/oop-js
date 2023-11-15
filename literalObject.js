2f7980e0942d0ea3ab8bc29029687c9dfd4f662f
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-15 09:39:57 -0500

    literal object

---
+++ literalObject.js
@@ -0,0 +1,19 @@
+console.log( {} );
+
+let name = "Ryan";
+let lastName = "Ray";
+let age = 30;
+
+const user = {
+//  keys & values
+    name: "Ryan",
+    lastName: "Ray",
+    age: 30,
+    hobbies: ['read', 'programming', 'run'],
+    address: {
+        street: "some street",
+        city: "London"
+    }
+}
+
+console.log(user);
